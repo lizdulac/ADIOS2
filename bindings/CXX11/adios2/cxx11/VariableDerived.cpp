5a6bf7a5f2d066012fc7c880b96b1e7e119237f4
anagainaru <ana.gainaru@gmail.com>
2023-10-23 08:46:48 -0400

    adding missing files

---
+++ bindings/CXX11/adios2/cxx11/VariableDerived.cpp
@@ -0,0 +1,12 @@
+#include "VariableDerived.h"
+
+#include "adios2/core/VariableDerived.h"
+
+namespace adios2
+{
+
+    VariableDerived::VariableDerived(core::VariableDerived *variable) : m_VariableDerived(variable)
+    {
+    }
+
+} // end namespace adios2
